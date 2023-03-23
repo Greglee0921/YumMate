f7c8dbbdbba957eb33ddf10e43a34203d6a28628
Greg Lee <greglee0921@gmail.com>
2023-03-23 16:17:17 -0400

    import changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 
 const queryClient = new QueryClient();
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
