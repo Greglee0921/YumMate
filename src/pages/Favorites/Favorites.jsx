9f21d052d10766cee0de25fc04a9e0ef827256fb
Greg Lee <greglee0921@gmail.com>
2023-03-23 16:25:45 -0400

    added div for empty favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -18,26 +18,34 @@ export const Favorites = () => {
       <h1 className="text-5xl text-link-hilight font-black mb-10 md:mb-20">
         Favorites
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 md:gap-10">
-        {favorites.map((recipe) => (
-          <div className="px-2 md:mb-0 mb-4" key={recipe.id}>
-            <Link to={`/recipe/${recipe.id}`} className="group">
-              <div className="h-min rounded-lg">
-                <div className="overflow-hidden rounded-lg ">
-                  <img
-                    className="object-cover rounded-lg aspect-square
+      {favorites ? (
+        <div className="grid grid-cols-2 md:grid-cols-4 md:gap-10">
+          {favorites.map((recipe) => (
+            <div className="px-2 md:mb-0 mb-4" key={recipe.id}>
+              <Link to={`/recipe/${recipe.id}`} className="group">
+                <div className="h-min rounded-lg">
+                  <div className="overflow-hidden rounded-lg ">
+                    <img
+                      className="object-cover rounded-lg aspect-square
                  group-hover:scale-125 transition-all duration-250"
-                    src={recipe.image}
-                  />
+                      src={recipe.image}
+                    />
+                  </div>
+                  <h1 className="text-xl mt-0 md:mt-3 group-hover:text-link-hilight">
+                    {recipe.title}
+                  </h1>
                 </div>
-                <h1 className="text-xl mt-0 md:mt-3 group-hover:text-link-hilight">
-                  {recipe.title}
-                </h1>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-xl font-bold text-nav">
+            No recipes saved yet. Search up recipes to add to your favorites!
+          </h1>
+        </div>
+      )}
     </div>
   );
 };
